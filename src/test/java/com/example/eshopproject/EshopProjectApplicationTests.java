b68087e2cfa4d29bc0e7d5c566720820cc27e4b4
leverpk <kamil.lewandowski7@gmail.com>
2022-02-03 14:08:10 +0100

    first commit

---
+++ src/test/java/com/example/eshopproject/EshopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eshopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
