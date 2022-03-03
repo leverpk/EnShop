057cfae6405f91c3524d07b744748455c203f011
leverpk <kamil.lewandowski7@gmail.com>
2022-03-03 10:09:55 +0100

    searched products done

--- src/main/java/com/example/eshopproject/model/Product.java
+++ src/main/java/com/example/eshopproject/model/Product.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.math.BigDecimal;
